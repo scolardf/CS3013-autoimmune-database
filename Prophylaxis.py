7a7c9cbdaeb64af290ee5065e115367d0c7602cc
Caolan <caolan.barry1@gmail.com>
2017-03-14 18:22:46 +0000

    adding comparrison between different prophylaxi

--- Prophylaxis.py
+++ Prophylaxis.py
@@ -2,6 +2,7 @@ import MySQLdb
 import matplotlib.pyplot as plt
 import numpy as np
 import decimal
+import string
 db = MySQLdb.connect(host="localhost",
                      user="root",
                      passwd="hellogoodbye",
@@ -12,35 +13,34 @@ cur = db.cursor()
 cur.execute("SELECT * FROM visit")
 ProphylaxisYesorNo = []
 indexValue = []
-yes = 0
-no = 0
 total = 0
 counter = 0
 yesArray = []
 noArray = []
+wordList = set()
 
 for row in cur.fetchall():
-    if (row[21] != None):
-        if (row[11] != ''):
-            ProphylaxisYesorNo.append(row[11])
-            indexValue.append(row[21])
-
-for x in range(0, len(ProphylaxisYesorNo)):
-    if (ProphylaxisYesorNo[x] == 'Yes'):
-        yes += 1
-        yesArray.append(indexValue[x])
+    if ((row[13]=='Non') or (row[13]=='No')):
+        noArray.append(row[21])
     else:
-        no += 1
-        noArray.append(indexValue[x])
+        yesArray.append(row[21])
+        wordArray = (row[13].split('+'))
+        for i in range(0, len(wordArray)):
+            wordList.add(string.strip(string.lower(wordArray[i])))
+
+wordList.remove('')
 
 yesAverage = decimal.Decimal(0)
-for y in range (0, len(yesArray)):
-    yesAverage+= decimal.Decimal(yesArray[y])
+for y in range(0, len(yesArray)):
+    if(yesArray[y]!=None):
+        yesAverage += decimal.Decimal(yesArray[y])
+
+noAverage = decimal.Decimal(0)
+for z in range(0, len(noArray)):
+    if(noArray[z]!=None):
+        noAverage += decimal.Decimal(noArray[z])
 
 yesAverage = yesAverage/y
-noAverage = decimal.Decimal(0)
-for z in range (0, len(noArray)):
-    noAverage = decimal.Decimal(noAverage)+decimal.Decimal(noArray[z])
 noAverage = noAverage/z
 #barchart
 width = 0.6
@@ -50,10 +50,12 @@ p1 = plt.bar(0.2, yesAverage, width, color = 'green')
 p2 = plt.bar(1, noAverage, width, color = 'red')
 
 plt.ylabel('Index average')
-plt.title('Average index of steroid and non-steroid users')
-plt.xticks(ind, ('Steroid Average', 'Non-Steroid Average'))
+plt.title('Average index of those issued prophylaxis vs. \n those not issued prophylaxis')
+plt.xticks(ind, ('Prophylaxis Average', 'No Prophylaxis Average'))
 plt.yticks(np.arange(0,0.9,0.1))
 print float(yesAverage)
 print float(noAverage)
-
+print len(noArray)
+print len(yesArray)
+print (wordList)
 plt.show()
